08b557914fc2ac924a5620f7f1aba842506c7200
john.clemente <john.clemente@johnclemente.foton.local>
2015-07-31 11:29:07 -0300

    Fim do item 3.2

--- src/br/com/casadocodigo/activity/DashboardActivity.java
+++ src/br/com/casadocodigo/activity/DashboardActivity.java
@@ -1,7 +1,28 @@
 package br.com.casadocodigo.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import br.com.casadocodigo.boaviagem.R;
 
-public class DashboardActivity extends Activity{
+public class DashboardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+
+	}
+
+	public void selecionarOpcao(View view) {
+		/*
+		 * com base na view que foi clicada iremos tomar a ação correta
+		 */
+		TextView textView = (TextView) view;
+		String opcao = "Opção: " + textView.getText().toString();
+		Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+	}
 
 }
